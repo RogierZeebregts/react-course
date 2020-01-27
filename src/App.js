a238c14c225f0580758aadd90ef34b730e1deca5
rzeebregts <r.zeebregts@juist.nl>
2020-01-27 16:43:18 +0100

    Div

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import Person from './components/Person/Person'
 import './App.css'
 
 class App extends Component {
     render () {
         return (
             <div className="App">
-                <h1>Hi</h1>
+                <h1>Hi, I'm a React App</h1>
+                <Person/>
             </div>
         )
     }
---
+++ src/components/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const person = (props) => {
+    return <p>PersonComponent </p>
+}
+
+export default person
