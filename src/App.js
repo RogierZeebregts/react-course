d95c7f4095d3e5a6a62657c1dc6c5b45207d5d6d
rzeebregts <r.zeebregts@juist.nl>
2020-01-29 17:08:18 +0100

    Div

--- src/App.js
+++ src/App.js
@@ -9,37 +9,78 @@ class App extends Component {
             {name: 'Ilse', age: 38},
             {name: 'Marc', age: 39},
         ],
+        showPersons: false,
     }
     
-    switchNameHandler = (newName) => {
+    nameChangedHandler = event => {
         this.setState({
             persons: [
-                {name: newName, age: 27},
-                {name: 'Nee', age: 21},
+                {name: 'Rogier', age: 27},
+                {name: event.target.value, age: 21},
                 {name: 'Marc', age: 39},
             ],
         })
     }
     
+    deletePersonHandler = (personIndex) => {
+        const persons = this.state.persons
+        persons.splice(personIndex, 1)
+        this.setState({persons: persons})
+    }
+    
+    togglePersonsHandler = () => {
+        this.setState({showPersons: !this.state.showPersons})
+    }
+    
     render () {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+        }
+        
+        let persons = null
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    {
+                        this.state.persons.map((person, index) => {
+                            return <Person
+                                name={person.name}
+                                age={person.age}
+                                click={_ => this.deletePersonHandler(index)}
+                            />
+                        })
+                    }
+                    
+                    {/*<Person*/}
+                    {/*    name={this.state.persons[0].name}*/}
+                    {/*    age={this.state.persons[0].age}*/}
+                    {/*/>*/}
+                    {/*<Person*/}
+                    {/*    name={this.state.persons[1].name}*/}
+                    {/*    age={this.state.persons[1].age}*/}
+                    {/*    click={_ => this.switchNameHandler('NAAM 2')}*/}
+                    {/*    changed={this.nameChangedHandler}*/}
+                    {/*>Child value</Person>*/}
+                    {/*<Person*/}
+                    {/*    name={this.state.persons[2].name}*/}
+                    {/*    age={this.state.persons[2].age}*/}
+                    {/*/>*/}
+                </div>
+            )
+        }
+        
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
-                <button onClick={this.switchNameHandler.bind(this, 'NAAM 1')}>Switch name</button> // Ineffeicient
+                <button
+                    style={style}
+                    onClick={this.togglePersonsHandler}>Toggle Persons
+                </button>
                 
-                <Person
-                    name={this.state.persons[0].name}
-                    age={this.state.persons[0].age}
-                />
-                <Person
-                    name={this.state.persons[1].name}
-                    age={this.state.persons[1].age}
-                    click={_ => this.switchNameHandler('NAAM 2')}
-                >Child value</Person>
-                <Person
-                    name={this.state.persons[2].name}
-                    age={this.state.persons[2].age}
-                />
+                {persons}
             </div>
         )
     }
