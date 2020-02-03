0731af870a0eed601fdbeb3db2b4860b4c68820e
rzeebregts <r.zeebregts@juist.nl>
2020-02-03 17:13:54 +0100

    Div

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import classes from './App.module.css'
-import {togglePersonsHandler} from '../components/Persons/Person/_helpers'
 import Persons from '../components/Persons/Persons'
+import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
     state = {
@@ -15,32 +15,21 @@ class App extends Component {
     
     render () {
         let persons = null
-        let btnClass = [classes.Button]
-        
         if (this.state.showPersons) {
             persons = (
                 <div>
                     <Persons persons={this.state.persons} this={this}/>
                 </div>
             )
-            
-            btnClass.push(classes.Red)
-        }
-        
-        const assignedClasses = []
-        if (this.state.persons.length <= 2) {
-            assignedClasses.push(classes.red)
-        }
-        if (this.state.persons.length <= 1) {
-            assignedClasses.push(classes.bold)
         }
         
         return (
             <div className={classes.App}>
-                <h1>Hi, I'm a React App</h1>
-                <p className={assignedClasses.join(' ')}>Dit is een paragraaf</p>
-                <button className={btnClass.join(' ')} onClick={_ => togglePersonsHandler(this)}>Toggle Persons</button>
-                
+                <Cockpit
+                    showPersons={this.state.showPersons}
+                    persons={this.state.persons}
+                    t={this}
+                />
                 {persons}
             </div>
         )
